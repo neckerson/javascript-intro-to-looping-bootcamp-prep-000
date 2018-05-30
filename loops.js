1d842513003253823a9671cd9798352dcbf355df
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-30 02:07:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,10 @@
 function forLoop(ary) {
   var array = [...ary];
   for (let i = 0; i < 25; i++) {
-  var s = i === 1 ? `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
-  array.push('s')
+    if (i === 1) {
+      array.push(`I am ${i} strange loops`)
+    } else {
+      array.push(`I am ${i} strange loop`)
+    }
   }
 }
